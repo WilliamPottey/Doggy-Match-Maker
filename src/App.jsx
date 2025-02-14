04d2f8c6f3bdf4460b6898a1ee0e2bc8c5554c6a
WilliamPottey <wottey9007@gmail.com>
2025-02-14 14:52:15 -0500

    change from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Dogs from "./Pages/Dogs";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter hashType="noslash">
         <Routes>
           <Route index element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dogs" element={<Dogs />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
